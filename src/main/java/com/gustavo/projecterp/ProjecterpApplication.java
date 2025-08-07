ec980c886bd5212878c0c979431d23801b8dc84d
Gustavo Oliveira <gustavo.h.oliveira7531@gmail.com>
2025-08-07 20:55:34 -0300

    project

---
+++ src/main/java/com/gustavo/projecterp/ProjecterpApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.projecterp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecterpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecterpApplication.class, args);
+	}
+
+}
